1e89ae9fa330a1c1b71e6a5850e726f8bb918e76
Sethmr <sethmr21@gmail.com>
2018-11-21 12:06:58 -0500

    added project

---
+++ Llorkcir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Llorkcir
+//
+//  Created by Seth Rininger on 11/21/18.
+//  Copyright © 2018 Vimvest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
